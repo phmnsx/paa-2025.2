cf211d5f0df7c9b3ceb532d51d60606f2c1626e0
phmnsx <lynnwastinghertime@gmail.com>
2025-12-07 00:58:55 -0300

    meio certo

--- sequenciamento/pedroantunes_202400018260_sequenciamento.c
+++ sequenciamento/pedroantunes_202400018260_sequenciamento.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 typedef struct a{
 	char* cod;
 	int percen;
 } nome;
 
-void calcularTabela(int* k, char* P){
+void calcularTabela(int* k, char* P, int offset){
+	for (int i = 0; i < strlen(P); i++)
+		k[i + offset] = -1;
 	for (int i = 1, j = -1; i < strlen(P); i++){
 		while (j >= 0 && P[j + 1] != P[i])
-			j = k[j];
+			j = k[j + offset];
 		if(P[j+1] == P[i])
 			j++;
-		k[i] = j;
+		k[i + offset] = j;
 	}
 }
 
@@ -24,26 +27,36 @@ int max(int a, int b){
 int kmpMod(int* k, char* T, char* P, int matchSize){
     int n = strlen(T);
     int m = strlen(P);
-
+    int nov = (int)round(m*0.9);
     int ind = -1, i = 0, j = -1, passos = 0;   
 
     while (i < n) {
-		printf("caracter: %c, j = %d, tabela = %d ind = %d\n", T[i], j + 1, k[j], ind);
-		if (ind >= m - 1)
-			return m;
+		//printf("caracter: %c, j = %d, tabela = %d ind = %d\n", T[i], j + 1, k[j], ind);
+		if (ind >= nov)
+			return ind + 1;
+		if (ind+ (n - i) < nov)
+			return ind + 1;
 		if(T[i] == P[j + 1]){
 			passos++;
+			i++;
 			j++;
 		}
 		//houve mismatch
 		else{
-			if(passos >= matchSize)
+			if(passos >= matchSize){
 				ind += passos;
-			j = max(k[j], ind);
+				if(ind < m - 1)
+					calcularTabela(k, &P[ind+1], ind+1);
+			}
+
+			if (j == -1 ||j == ind)
+				i++;
+			if ((j >= ind + 1) && j >= 0)
+				j = ind + 1 + k[j];
+			//printf("%d\n", j);
 			//j = ind; // j = indice maior entre: indice "normal" ou indice minimo permitido (se ind avançar, alguma parte ja teve match valido antes);
-			passos = 0;
+			passos = j + 1 - (ind + 1);
 		}
-		i++;
     }
 
     return ind + 1;	
@@ -51,43 +64,32 @@ int kmpMod(int* k, char* T, char* P, int matchSize){
 
 int main(int argc, char* argv[]){
 	FILE* input = fopen(argv[1], "r");
-    FILE* output = fopen(argv[2], "w");
+    //FILE* output = fopen(argv[2], "w");
     int matchSize;
     fscanf(input, "%d", &matchSize);
-    char buffer[4096];
+    char buffer[100000];
     char* entrada;
 	fscanf(input, "%s", buffer);
+	int n = strlen(buffer);
+	printf("%ld\n", n);
 	entrada = malloc(sizeof(char)*strlen(buffer) + 1);
 	strcpy(entrada, buffer);
 	int tamanho;
 	int tamanho2;
-	float valid;
-	char* cadeia;
-	int tabela[4096];
+	//float valid;
+	int* tabela = (int*) malloc(sizeof(int)*4096);
 	fscanf(input, "%d", &tamanho);
 	nome* list = malloc(sizeof(nome)*tamanho);
 	for(int i = 0; i < tamanho; i++){
-		valid = 0.0;
 		fscanf(input, "%s", buffer);
 		list[i].cod = malloc(sizeof(char)*strlen(buffer) + 1);
 		strcpy(list[i].cod, buffer);
 		fscanf(input, "%d", &tamanho2);
 		for(int j = 0; j < tamanho2; j++){
 			fscanf(input, "%s", buffer);
-			cadeia = malloc(sizeof(char)*strlen(buffer) + 1);
-			strcpy(cadeia, buffer);
-			for(int k = 0; k < strlen(cadeia); k++){
-				tabela[k] = -1;
-			}
-			calcularTabela(tabela, cadeia);
-			printf("cad: %s, %d\n", cadeia, strlen(cadeia));
-			printf("tabela:");
-			for(int k = 0; k < strlen(cadeia); k++){
-				printf("%i, ", tabela[k]);
-			}
-			printf("\n");
-			int resps = kmpMod(tabela, entrada, cadeia, matchSize);
-			printf("resp:%d\n\n", resps);
+			calcularTabela(tabela, buffer, 0);
+			int resps = kmpMod(tabela, entrada, buffer, matchSize);
+			printf("%d\n", resps);
 		}
 	}
 }
